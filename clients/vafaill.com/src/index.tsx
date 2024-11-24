954404e8dc9ce948c9a5ccd68661d6c86af0c0a9
KahlerYasla <garpayyasla@gmail.com>
2024-11-24 21:37:22 +0300

    update:clients: home container view changes

--- clients/vafaill.com/src/index.tsx
+++ clients/vafaill.com/src/index.tsx
@@ -2,8 +2,8 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { LayoutContainer } from "./modules/layout"
 import { HomeContainer } from "./modules/home"
+import { LayoutContainer } from "./modules/layout"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
